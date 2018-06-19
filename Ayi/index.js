0ab604cf7e3126719e293758719de091bfdb53d6
wcy92929292 <1035773609@qq.com>
2018-06-19 18:15:39 +0800

    alert

--- Ayi/index.js
+++ Ayi/index.js
@@ -34,12 +34,16 @@
 					
 	
 	$(".xmay input").click(function(){   
+		$("#iden").html("您的身份选择为 <b>雇主</b>，提交之后无法修改");
     	check_xmWho(1);
+    	
 		$("#gzInfo").show();
 		$("#ayiInfo").hide();
     });
-    $(".xmgz input").click(function(){   
+    $(".xmgz input").click(function(){  
+    	$("#iden").html("您的身份选择为 <b>阿姨</b>，提交之后无法修改");
     	check_xmWho(2);
+    	
 		$("#gzInfo").hide();
 		$("#ayiInfo").show();
     });
@@ -49,6 +53,7 @@
     var xmWho_int = 0;					//当等于1的时候 是寻觅阿姨 如果等于2 就是寻觅工作
     check_xmWho(1);
     function check_xmWho(who_int){
+    	$('#my-alert').modal('open');
     	if(who_int==1){
     		$(".xmgz input").removeAttr("checked");
     		$(".xmay input").prop("checked",function(){
@@ -207,6 +212,10 @@
 		}
 		function cbGetAyi(resp){
 		   console.log("response of push:"+JSON.stringify(resp.result));
+		   $('#my-modal-loading').modal('open');
+		   setTimeout(function(){
+			   $('#my-modal-loading').modal('close');
+		   },2000);
 		   
 		   if(typeof resp == 'string'){
 				console.log('reject');
@@ -220,6 +229,7 @@
 						 }else{
 							imgurl = "厨师.png";
 						 }
+						 $("#list").html("");
 						$("#list").append('<div class="swiper-slide">'+
 										'<ul><li>'+			            					              						              		
 											'<div class="ayi-box">'+
@@ -271,6 +281,7 @@
 						 }else{
 							imgurl = "厨师.png";
 						 }
+						 $("#list").html("");
 						$("#list").append('<div class="swiper-slide">'+
 										'<ul><li>'+			            					              						              		
 											'<div class="ayi-box">'+
@@ -325,6 +336,7 @@
 						 }else{
 							imgurl = "厨师.png";
 						 }
+						 $("#list").html("");
 						$("#list").append('<div class="swiper-slide">'+
 										'<ul><li>'+			            					              						              		
 											'<div class="ayi-box">'+
